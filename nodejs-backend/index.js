1b704e4ab2e9f1fdfe21f5c539ab7f0c4b53f322
Sukethksrivatsan <122083441+Sukethksrivatsan@users.noreply.github.com>
2023-01-06 14:39:06 +0530

    Create index.js

---
+++ nodejs-backend/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+const ipl_model = require('./ipl_model');
+
+app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+app.post('/login', (req, res) => {
+  ipl_model.login(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+});
+
+app.post('/executeQuery', (req, res) => {
+  ipl_model.executeQuery(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
